3155dbfd48a9e169fd04a5e7af2c55f518621220
PRASADPAMIDI579 <prasadpamidi579@gmail.com>
2024-03-03 00:02:55 +0530

    added new data in vars

--- 02-vars/outputs.tf
+++ 02-vars/outputs.tf
@@ -1,3 +1,11 @@
 output "sample" {
   value = var.sample
 }
+
+output "sample2" {
+  value = var.sample2
+}
+
+output "sample3" {
+  value = var.sample3
+}
--- 02-vars/vars.tf
+++ 02-vars/vars.tf
@@ -4,3 +4,12 @@ variable "sample" {
   default = "Hello Terraform"
 }
 
+#number data type
+variable "sample2" {
+  default = 100
+}
+
+#Boolean data type
+variable "sample3" {
+  default = TRUE
+}
